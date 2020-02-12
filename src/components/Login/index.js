540bf1d584d8657e1717816e32ae22cd8bb4d689
c.saclao <c.saclao@WSC-FDC-EF5C.g07.fujitsu.local>
2020-02-12 16:18:00 +0800

    design login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,14 +1,69 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import ArrowDropDown from "@material-ui/icons/ArrowDropDown";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import TextField from "@material-ui/core/TextField";
 
 class Login extends React.Component {
   render() {
-    
     return (
       <div>
-          <h1>
-              LOGIN
-          </h1>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
+              Fujitsu Automation Voting System
+            </Typography>
+            <IconButton
+              aria-label="display more actions"
+              edge="end"
+              color="inherit"
+            >
+              English
+              <ArrowDropDown />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Grid
+          container
+          spacing={3}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={3} sm={3}>
+            <Paper style={{ marginTop: 50 }}>
+              <Card>
+                <CardContent>
+                  <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                  >
+                    <AccountCircle color="disabled" style={{ fontSize: 120 }} />
+                  </Grid>
+                  <TextField label="Username" fullWidth />
+                  <br />
+                  <TextField label="Password" fullWidth />
+                  <br />
+                  <br />
+                  <Button variant="contained" color="primary">
+                    Login
+                  </Button>
+                </CardContent>
+              </Card>
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     );
   }
@@ -16,13 +71,13 @@ class Login extends React.Component {
 const mapStateProps = state => {
   return {
     state
-  }
+  };
 };
 const mapDispatch = dispatch => {
   return {};
 };
 const connector = connect(
   mapStateProps,
-  mapDispatch,
+  mapDispatch
 );
 export default connector(Login);
